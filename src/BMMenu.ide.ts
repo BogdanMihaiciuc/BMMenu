ddc76b5359c2b6cb6487bac399a165a297aad15f
Bogdan Mihaiciuc <bmihaiciuc@ptc.com>
2019-08-26 13:42:24 +0300

    initial commit

---
+++ src/BMMenu.ide.ts
@@ -0,0 +1,136 @@
+// automatically import the css file
+import { ThingworxComposerWidget, TWWidgetDefinition, description, autoResizable, property, defaultValue, bindingTarget, baseTypeInfotableProperty, event, service, nonEditable, bindingSource, hidden } from 'typescriptwebpacksupport';
+
+@description('Displays a popup menu when the user right-clicks on a region or a different widget.')
+@TWWidgetDefinition('Popup Menu', autoResizable)
+class BMMenuWidget extends TWComposerWidget {
+
+    @property ('NUMBER', defaultValue(90)) Width: number;
+
+    @property ('NUMBER', defaultValue(30)) Height: number;
+
+    @description('The data source kind to use.')
+    @property ('STRING', defaultValue('state')) dataSourceKind: string;
+
+    @description('When the data source is a state definition, this represents the menu to use for this popup menu.')
+    @property ('STATEDEFINITION') menuDefinition: string;
+
+    @description('When the data source is an infotable, this represents the menu to use for this popup menu.')
+    @property ('INFOTABLE', bindingTarget) menu: any;
+
+    @description('The field that represents the name of the menu entry.')
+    @property ('FIELDNAME', baseTypeInfotableProperty('menu')) nameField: string;
+
+    @description('Controls what area triggers the popup menu on right click.')
+    @property ('STRING', defaultValue('thisWidget')) targetKind: string;
+
+    @description('When the target is set to another widget, this represents the display name of the widget on which right-clicking will trigger the popup menu.')
+    @property ('STRING') targetWidget: string;
+
+    @description('Triggered when the user clicks on a menu item.')
+    @event menuDidSelectItem;
+
+    @description('Represents the name of the menu item that was clicked')
+    @property ('STRING', nonEditable, bindingSource) selectedMenuItem;
+
+    @property ('STRING', defaultValue('{}'), hidden) private _menuDefinition;
+
+    @description('Should be invoked to show the menu. It will be shown from the bottom-right corner of its target.')
+    @service showMenu;
+
+    widgetIconUrl(): string {
+        return require('./images/icon.png');
+    }
+
+    widgetProperties(): TWWidgetProperties {
+        require("./styles/ide.css");
+        const props = super.widgetProperties();
+
+        props.properties.dataSourceKind.selectOptions = [{value: 'state', text: 'State Definition'}, {value: 'infotable', text: 'Infotable'}];
+        props.properties.targetKind.selectOptions = [{value: 'thisWidget', text: 'This Widget'}, {value: 'reference', text: 'Other Widget'}];
+
+        return props;
+    }
+
+    renderHtml(): string {
+        return '<div class="widget-content BMMenu">Menu: Invisible at runtime</div>';
+    };
+
+    afterSetProperty(key: string, value: any): any {
+        if (key == 'menuDefinition') {
+            this.menuDefinitionDidChange();
+        }
+    }
+
+    afterLoad() {
+		var properties = this.allWidgetProperties().properties;
+		
+		// Retrieve the menu definition and generate the events for that menu
+		var menuDefinition = JSON.parse(this.getProperty('_menuDefinition'));
+		for (var i = 0; i < menuDefinition.length; i++) {
+			properties['Menu:' + menuDefinition[i]] = <any>{
+				isBaseProperty: false,
+				name: 'Menu:' + menuDefinition[i],
+				type: 'event',
+				isVisible: true,
+				description: 'Triggered when selecting the ' + menuDefinition[i] + ' menu entry on a cell.',
+				_BMCategories: ['all', 'menu']
+			};
+		}
+		
+		// Update the properties UI
+		this.updatedProperties();
+    }
+
+    /**
+     * Invoked upon the menu definition changing.
+     */
+    menuDefinitionDidChange() {
+        const properties = this.allWidgetProperties().properties;
+
+        const definition = TW.getStateDefinition(this.getProperty('menuDefinition'));
+
+        const oldDefinitions: string[] = JSON.parse(this.getProperty('_menuDefinition') || '[]');
+
+        for (const definition of oldDefinitions) {
+            delete properties['Menu:' + definition];
+        }
+
+        let stateDefinition: any;
+		
+		if (!(stateDefinition = (definition && definition.content && definition.content.stateDefinitions))) {
+			// If the state definition is undefined, there is nothing else to do
+			return;
+        }
+		
+		// Extract the state names from the menu definition
+        var menuDefinition: any[] = [];
+        for (const definition of stateDefinition) {
+			if (definition.name) menuDefinition.push(definition.name);
+        }
+
+		this.setProperty('_menuDefinition', JSON.stringify(menuDefinition));
+		
+		// Append the new properties to this widget
+		for (var i = 0; i < menuDefinition.length; i++) {
+			properties['Menu:' + menuDefinition[i]] = {
+				isBaseProperty: false,
+				name: 'Menu:' + menuDefinition[i],
+				type: 'event',
+				isVisible: true,
+				description: 'Triggered when selecting the ' + menuDefinition[i] + ' menu entry.',
+				_BMCategories: ['all', 'menu']
+			} as any;
+		}
+	
+		// Update the properties UI
+		this.updatedProperties();
+    }
+
+    afterRender(): void {
+    }
+
+    beforeDestroy(): void {
+    }
+
+}
---
+++ src/BMMenu.runtime.ts
@@ -0,0 +1,196 @@
+/// <reference path="/Users/bmihaiciuc/OneDrive - PTC/BMCoreUI/ui/BMCoreUI/BMCoreUI.d.ts"/>
+
+import { ThingworxRuntimeWidget, TWService, TWProperty } from 'typescriptwebpacksupport'
+
+/**
+ * An enum containing the kinds of data sources that this widget can use.
+ */
+export enum BMMenuWidgetDataSourceKind {
+    /**
+     * Represents that the data source is a state definition.
+     */
+    State = 'state',
+
+    /**
+     * Represents that the data source is an infotable.
+     */
+    Infotable = 'infotable'
+}
+
+/**
+ * An enum containing the kinds of targets that this widget can use.
+ * The target is the area of the viewport that can be used to trigger this menu.
+ */
+export enum BMMenuWidgetTargetKind {
+
+    /**
+     * Represents that the target is this widget.
+     */
+    ThisWidget = 'thisWidget',
+
+    /**
+     * Represents that the target is a different widget within the mashup.
+     */
+    Reference = 'reference'
+}
+
+@ThingworxRuntimeWidget
+export class BMMenuWidget extends TWRuntimeWidget {
+
+    /**
+     * The data source kind to use.
+     */
+    @TWProperty ('dataSourceKind') dataSourceKind: BMMenuWidgetDataSourceKind;
+
+    /**
+     * Represents the complete menu definition when the data source is a state definition.
+     */
+    @TWProperty ('menuDefinition') menuDefinition: string;
+
+    /**
+     * Represents the infotable field to use as the name when the data source is an infotable.
+     */
+    @TWProperty ('nameField') nameField: string;
+
+    /**
+     * The menu infotable.
+     */
+    @TWProperty('menu') 
+    set menuInfotable(menuInfotable: TWInfotable) {
+        this.menu = undefined;
+
+        // Clear out the menu if the infotable is blank
+        if (!menuInfotable) return;
+
+        // Recreate the menu whenever this infotable gets updated
+        const items = menuInfotable.rows.map(e => BMMenuItem.menuItemWithName(e[this.nameField]));
+
+        if (!items.length) return;
+
+        this.menu = BMMenu.menuWithItems(items);
+        this.menu.delegate = this;
+    }
+
+    /**
+     * The kind of target.
+     */
+    @TWProperty ('targetKind') targetKind: BMMenuWidgetTargetKind;
+
+    /**
+     * The target widget name, if the target kind is a widget reference.
+     */
+    @TWProperty ('targetWidget') targetWidgetReference: string;
+
+    @TWProperty ('selectedMenuItem') selectedMenuItem: string;
+
+    /**
+     * The target widget, if the target kind is a widget reference and the widget could be found.
+     */
+    targetWidget?: TWWidget;
+
+
+    /**
+     * The menu instance.
+     */
+    menu?: BMMenu;
+
+    /**
+     * The context menu handler to attach to widgets.
+     */
+    contextMenuHandler: (event?: MouseEvent) => void;
+
+    renderHtml(): string {
+        return '<div class="widget-content BMMenuWidget"></div>';
+    };
+
+    async afterRender(): Promise<void> {
+        if (this.dataSourceKind == BMMenuWidgetDataSourceKind.State) {
+            const items = [];
+
+            // When data source is set to a state definition, extract the states and build the menu items
+            const stateDefinition = TW.getStateDefinition(this.menuDefinition);
+            const states = stateDefinition && stateDefinition.content && stateDefinition.content.stateDefinitions;
+
+            if (!states || !states.length) return;
+
+            for (const state of states) {
+                if (state.name) items.push(BMMenuItem.menuItemWithName(state.name));
+            }
+
+            // Also initialize the menu
+            this.menu = BMMenu.menuWithItems(items);
+
+            this.menu.delegate = this;
+        }
+
+        this.contextMenuHandler = event => {
+            if (!this.targetWidget) return;
+
+            const frame = BMRectMakeWithNodeFrame(this.targetWidget.jqElement[0]);
+            const point = BMPointMake();
+
+            if (event) {
+                event.preventDefault();
+                point.x = event.pageX;
+                point.y = event.pageY;
+            }
+            else {
+                point.x = frame.bottom;
+                point.y = frame.bottom;
+            }
+
+            if (this.menu) {
+                this.menu.openAtPoint(point);
+            }
+        }
+
+        // For infotable data sources, await for the value to be initialized
+
+        // If the target kind is a widget reference, wait for the mashup to finish loading, then attempt to find that widget
+        if (this.targetKind == BMMenuWidgetTargetKind.Reference) {
+            const widgetName = this.targetWidgetReference;
+            this.boundingBox[0].style.display = 'none';
+
+            await 0;
+
+            this.targetWidget = this.findWidgetNamed(widgetName, {inWidget: this.mashup.rootWidget});
+        }
+        else {
+            this.targetWidget = this;
+        }
+
+        if (this.targetWidget) this.targetWidget.jqElement[0].addEventListener('contextmenu', this.contextMenuHandler);
+    }
+
+    /**
+     * Finds the widget with the given name, starting from the given widget.
+     * This method will recursively look through the child widgets of the specified widget.
+     * @param named         The name of the widget.
+     * @param inWidget      The widget to search through.
+     */ 
+    private findWidgetNamed(named, {inWidget: widget}: {inWidget: TWRuntimeWidget}) {
+        if (widget.getProperty('DisplayName') == named) return widget;
+
+        let result: TWRuntimeWidget | undefined;
+        for (const child of widget.getWidgets()) {
+            result = this.findWidgetNamed(named, {inWidget: child});
+            if (result) return result;
+        }
+    }
+
+    menuDidSelectItem(menu: BMMenu, item: BMMenuItem) {
+        this.selectedMenuItem = item.name;
+        this.jqElement.triggerHandler('menuDidSelectItem');
+        this.jqElement.triggerHandler('Menu:' + item.name);
+    }
+
+    @TWService('ShowMenu')
+    showMenu() {
+        this.contextMenuHandler();
+    }
+
+    beforeDestroy?(): void {
+        // resetting current widget
+        if (this.targetWidget) this.targetWidget.jqElement[0].removeEventListener('contextmenu', this.contextMenuHandler);
+    }
+}
